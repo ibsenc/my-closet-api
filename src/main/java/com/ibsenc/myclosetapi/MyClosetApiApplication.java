f5704607e9666b0621b8b7ed71d6ed7550609725
ibsenc <ibsen.camille@gmail.com>
2021-08-28 20:04:47 -0700

    Added generated spring-boot project

---
+++ src/main/java/com/ibsenc/myclosetapi/MyClosetApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibsenc.myclosetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClosetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyClosetApiApplication.class, args);
+	}
+
+}
